883993b4fc99630447b336e25cc3cfcc9932d1ae
Zack <schaxmann@gmail.com>
2022-10-08 22:36:16 +0100

    Fixes sidebar navigation icons

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -21,7 +21,7 @@ import {
 const navMenu = [
   { name: "Home", icon: MdHome, route: "/" },
   { name: "Search", icon: MdSearch, route: "/search" },
-  { name: "Your Library", icon: MdSearch, route: "/library" },
+  { name: "Your Library", icon: MdLibraryMusic, route: "/library" },
 ];
 
 const Sidebar = () => {
