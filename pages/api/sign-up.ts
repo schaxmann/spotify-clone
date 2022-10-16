229c89cc249c364ac28a6601450630ad6bcd815b
Zack <schaxmann@gmail.com>
2022-10-16 21:54:47 +0100

    Adds sign-up functionality to sign-up endpoint

--- pages/api/sign-up.ts
+++ pages/api/sign-up.ts
@@ -3,7 +3,45 @@ import jwt from "jsonwebtoken";
 import cookie from "cookie";
 import prisma from "../../lib/prisma";
 import { NextApiRequest, NextApiResponse } from "next";
+import { useStoreRehydrated } from "easy-peasy";
+import { LayoutGroupContext } from "framer-motion";
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
   res.status(200).json({ page: "sign-up" });
+  const salt = bcrypt.genSaltSync;
+  const { email, password } = req.body;
+  let user;
+  try {
+    user = await prisma.user.create({
+      data: {
+        email,
+        password: bcrypt.hashSync(password, salt),
+      },
+    });
+  } catch (e) {
+    res.status(401);
+    res.json({ error: "User already exists" });
+  }
+
+  const token = jwt.sign(
+    {
+      email: user.email,
+      id: user.id,
+      time: Date.now(),
+    },
+    "hello",
+    { espiresIn: "8h" }
+  );
+  res.setHeader(
+    "Set-Cookie",
+    cookie.serialize("NOTIFY-ACCESS", token, {
+      httpOnly: true,
+      maxAge: 8 * 60 * 60,
+      path: "/",
+      sameSite: "lax",
+      secure: process.env.NODE_ENV === "production",
+    })
+  );
+
+  res.json(user);
 };
