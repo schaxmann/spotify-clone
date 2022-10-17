2b26daa45d6a1804a9de14f79ca1d5174d2d12ec
Zack <schaxmann@gmail.com>
2022-10-17 23:36:17 +0100

    Adds ternary to app.tsx, hide player on auth pages

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -33,9 +33,13 @@ const theme = extendTheme({
 function MyApp({ Component, pageProps }) {
   return (
     <ChakraProvider theme={theme}>
-      <PlayerLayout>
+      {Component.authpage ? (
         <Component {...pageProps} />
-      </PlayerLayout>
+      ) : (
+        <PlayerLayout>
+          <Component {...pageProps} />
+        </PlayerLayout>
+      )}
     </ChakraProvider>
   );
 }
