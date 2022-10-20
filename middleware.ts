2a048165ca0545698f85b3ef288cffc446dc206b
Zack <schaxmann@gmail.com>
2022-10-20 23:59:09 +0100

    Atempt middleware fix

--- middleware.ts
+++ middleware.ts
@@ -9,9 +9,27 @@ export default function middleware(req) {
     const token = req.cookies.NOTIFY_ACCESS;
 
     if (!token) {
-      return NextResponse.rewrite(`${origin}/sign-in`);
+      console.log("na");
     } else {
-      return NextResponse.rewrite(`${origin}/`);
+      return NextResponse.rewrite(`/`);
     }
   }
 }
+
+// import { NextResponse } from "next/server";
+
+// const securePages = ["/", "/playlist", "/library"];
+
+// export default function middleware(req) {
+//   const { pathname, origin } = req.nextUrl;
+
+//   if (securePages.find((pages) => pages === req.nextUrl.pathname)) {
+//     const token = req.cookies.NOTIFY_ACCESS;
+
+//     if (!token) {
+//       return NextResponse.rewrite(`${origin}/sign-in`);
+//     } else {
+//       return NextResponse.rewrite(`/`);
+//     }
+//   }
+// }
